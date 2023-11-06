2ad9a78a170cee13cd083df96a511454230e1629
viktor <gjiuhhbiu21@gmail.com>
2023-11-06 23:43:10 +0300

    added preparedStatement

--- src/main/java/hexlet/code/Main.java
+++ src/main/java/hexlet/code/Main.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -16,12 +17,18 @@ public class Main {
             statement.execute(sql);
         } // В конце закрываем
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        try (var statement2 = conn.createStatement()) {
-            statement2.executeUpdate(sql2);
+        var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+        try (var statement2 = conn.prepareStatement(sql2)) {
+            statement2.setString(1, "Tommy");
+            statement2.setString(2, "111111");
+            statement2.executeUpdate();
+
+            statement2.setString(1, "Johny");
+            statement2.setString(2, "222222");
+            statement2.executeUpdate();
         }
 
-        var sql3 = "SELECT * FROM users";
+        /*var sql3 = "SELECT * FROM users";
         try (var statement3 = conn.createStatement()) {
             // Здесь вы видите указатель на набор данных в памяти СУБД
             var resultSet = statement3.executeQuery(sql3);
@@ -31,9 +38,29 @@ public class Main {
                 System.out.println(resultSet.getString("username"));
                 System.out.println(resultSet.getString("phone"));
             }
-        }
+        }*/
+        printUsers(conn);
+        System.out.println();
 
+        var deleteSql = "DELETE FROM users WHERE username=?";
+        try (var DelState = conn.prepareStatement(deleteSql)) {
+            DelState.setString(1, "Johny");
+            DelState.executeUpdate();
+        }
+        printUsers(conn);
         // Закрываем соединение
         conn.close();
+    }
+    public static void printUsers(Connection conn) throws SQLException {
+        var sql3 = "SELECT * FROM users";
+        try (var statement3 = conn.createStatement()) {
+            // Здесь вы видите указатель на набор данных в памяти СУБД
+            var resultSet = statement3.executeQuery(sql3);
+            // Набор данных — это итератор
+            // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
+            while (resultSet.next()) {
+                System.out.printf("%s %s\n", resultSet.getString("username"),resultSet.getString("phone"));
+            }
+        }
     }
 }
