a8f208d96b14a1345ef137a19b66109b838b13e0
viktor <gjiuhhbiu21@gmail.com>
2023-11-07 21:53:11 +0300

    added DA0

--- src/main/java/hexlet/code/Main.java
+++ src/main/java/hexlet/code/Main.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.user.UserDA0;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,43 +12,27 @@ public class Main {
         // База создается прямо во время выполнения этой строчки
         // Здесь hexlet_test — это имя базы данных
         var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
-
         var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
         // Чтобы выполнить запрос, создадим объект statement
         try (var statement = conn.createStatement()) {
             statement.execute(sql);
         } // В конце закрываем
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
-        try (var statement2 = conn.prepareStatement(sql2)) {
-            statement2.setString(1, "Tommy");
-            statement2.setString(2, "111111");
-            statement2.executeUpdate();
+        var tommy = new User("Tommy", "111111");
+        var johny = new User("Johny", "222222");
 
-            statement2.setString(1, "Johny");
-            statement2.setString(2, "222222");
-            statement2.executeUpdate();
-        }
+        var userDa0 = new UserDA0(conn);
 
-        /*var sql3 = "SELECT * FROM users";
-        try (var statement3 = conn.createStatement()) {
-            // Здесь вы видите указатель на набор данных в памяти СУБД
-            var resultSet = statement3.executeQuery(sql3);
-            // Набор данных — это итератор
-            // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("username"));
-                System.out.println(resultSet.getString("phone"));
-            }
-        }*/
+        userDa0.save(tommy);
+        userDa0.save(johny);
         printUsers(conn);
         System.out.println();
 
-        var deleteSql = "DELETE FROM users WHERE username=?";
-        try (var DelState = conn.prepareStatement(deleteSql)) {
-            DelState.setString(1, "Johny");
-            DelState.executeUpdate();
-        }
+        System.out.println(userDa0.find(1L));
+        System.out.println(userDa0.find(2L));
+        System.out.println(userDa0.find(3L));
+
+        userDa0.delete(2L);
         printUsers(conn);
         // Закрываем соединение
         conn.close();
---
+++ src/main/java/hexlet/code/user/UserDA0.java
@@ -0,0 +1,67 @@
+package hexlet.code.user;
+
+import hexlet.code.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+// Импорт User
+
+public class UserDA0 {
+    private Connection connection;
+
+    public UserDA0(Connection conn) {
+        connection = conn;
+    }
+
+    public void save(User user) throws SQLException {
+        // Если пользователь новый, выполняем вставку
+        // Иначе обновляем
+        if (user.getId() == null) {
+            var sql = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, user.getUsername());
+                preparedStatement.setString(2, user.getPhone());
+                preparedStatement.executeUpdate();
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+                // Если идентификатор сгенерирован, извлекаем его и добавляем в сохраненный объект
+                if (generatedKeys.next()) {
+                    // Обязательно устанавливаем id в сохраненный объект
+                    user.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving an entity");
+                }
+            }
+        } else {
+            // Здесь код обновления существующей записи
+        }
+    }
+
+    // Возвращается Optional<User>
+    // Это упрощает обработку ситуаций, когда в базе ничего не найдено
+    public Optional<User> find(Long id) throws SQLException {
+        var sql = "SELECT * FROM users WHERE id = ?";
+        try (var stmt = connection.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                var username = resultSet.getString("username");
+                var phone = resultSet.getString("phone");
+                var user = new User(username, phone);
+                user.setId(id);
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        }
+    }
+
+    // Удаляем пользователя
+    public void delete(Long id) throws SQLException {
+        var deleteSql = "DELETE FROM users WHERE id=?";
+        try (var DelState = connection.prepareStatement(deleteSql)) {
+            DelState.setLong(1, id);
+            DelState.executeUpdate();
+        }
+    }
+}
